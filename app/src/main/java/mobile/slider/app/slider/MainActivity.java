c02fb8994c078e0733d056245fc3b8f143452285
Nicolas <ruiznico360@gmail.com>
2017-03-20 09:54:18 -0400

    commit

--- app/src/main/java/mobile/slider/app/slider/MainActivity.java
+++ app/src/main/java/mobile/slider/app/slider/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String s = "";
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
