8ddd0a2bc1e3f9a614d4b68bed2b0e889e7f8534
ruiznico360 <ruiznico360@gmail.com>
2017-11-29 13:22:32 -0500

    Update ContentFragment.java

--- app/src/main/java/mobile/slider/app/slider/content/ContentFragment.java
+++ app/src/main/java/mobile/slider/app/slider/content/ContentFragment.java
@@ -25,7 +25,7 @@ public class ContentFragment extends Fragment {
         f.layoutResourceView = v;
         return f;
     }
-    //hi there b
+    //hi there B
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v;
